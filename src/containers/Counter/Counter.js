3fb0f66d394bafe1ff58e36440e5ecad2ea64210
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-16 12:50:24 -0400

    Updating State Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -25,6 +25,15 @@ class Counter extends Component {
           label="Subtract 5"
           clicked={() => this.props.onSubtractCounter("sub", 5)}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => (
+            <li key={strResult.id} onClick={this.props.onDeleteResult}>
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -32,7 +41,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -41,7 +51,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", value: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 15 })
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,32 +1,44 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
+      ...state,
+
       counter: state.counter + 1
     };
   }
   if (action.type === "DECREMENT") {
     return {
+      // ... clones old state with all properties
+      ...state,
       counter: state.counter - 1
     };
   }
   if (action.type === "ADD") {
     return {
+      ...state,
+
       counter: state.counter + action.value
     };
   }
   if (action.type === "SUBTRACT") {
     return {
+      ...state,
+
       counter: state.counter - action.value
     };
   }
-
-  //   DECREMENT;
-  //   ADD;
-  //   SUBTRACT;
+  if (action.type === "STORE_RESULT") {
+    return {
+      ...state,
+      // concat returns a new array by adding a new item
+      results: state.results.concat({ id: new Date(), value: state.counter })
+    };
+  }
 
   return state;
 };
