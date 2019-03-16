5611d9d56452ac52d602642d147748112291fe4f
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-16 11:03:38 -0400

    added reducer logic

--- redux-basics.js
+++ redux-basics.js
@@ -1,25 +1,29 @@
-const redux = require('redux');
+const redux = require("redux");
 const createStore = redux.createStore;
 
 const initialState = {
-    counter: 0
-}
+  counter: 0
+};
 
 // Reducer
+// rootReducer is the name we chose for the reducer
+// action returns the updated state
+// reducer changes the state manipulation data of the project
+
 const rootReducer = (state = initialState, action) => {
-    if (action.type === 'INC_COUNTER') {
-        return {
-            ...state,
-            counter: state.counter + 1
-        };
-    }
-    if (action.type === 'ADD_COUNTER') {
-        return {
-            ...state,
-            counter: state.counter + action.value
-        };
-    }
-    return state;
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  return state;
 };
 
 // Store
@@ -27,11 +31,12 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Subscription
+// subscriptions make sure you don't have to manually call getState(). It informs you when you need to get a new state when something changes
 store.subscribe(() => {
-    console.log('[Subscription]', store.getState());
+  console.log("[Subscription]", store.getState());
 });
 
 // Dispatching Action
-store.dispatch({type: 'INC_COUNTER'});
-store.dispatch({type: 'ADD_COUNTER', value: 10});
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// provider passes info to the rest of the app. In this case reducer, which contains the state
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
