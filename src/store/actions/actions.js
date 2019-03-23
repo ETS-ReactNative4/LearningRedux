a4c0efdd8d20461a1da4bb57820ba755df60e930
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-23 09:18:52 -0400

    Handling Asynchronous Code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -31,10 +31,21 @@ export const subtract = value => {
   };
 };
 
-export const storeResult = res => {
+export const saveResust = res => {
   return {
     type: STORE_RESULT,
     result: res
+  };
+};
+
+export const storeResult = res => {
+  // thunk will give us the dispatch
+  //middleware runs between the dispatch and before the action reaches the reducer
+  return dispatch => {
+    setTimeout(() => {
+      // we need to use a synchronus action creator in an a-synchronus function
+      dispatch(saveResust(res));
+    }, 2000);
   };
 };
 
